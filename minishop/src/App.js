700e146df5a706bfcfd77438b35d6e5d96d04636
sachin_yadav <sy9084087@gmail.com>
2023-01-14 19:12:37 +0530

    start

--- minishop/src/App.js
+++ minishop/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+ 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+    <h1>DO NOT PUSH ANY THING AS PER NOW</h1>
     </div>
   );
 }
