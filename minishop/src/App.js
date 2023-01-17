1bb0e97d6cea7f472799ce2fc3768da422d912f1
kiranwankhade <kiranwankhade7738@gamil.com>
2023-01-17 20:43:13 +0530

    day1-01

--- minishop/src/App.js
+++ minishop/src/App.js
@@ -1,5 +1,6 @@
  
 import './App.css';
+import SinglePage from './Pages/SinglePage';
 
 function App() {
   return (
---
+++ minishop/src/redux/SingleProducts/SingleProduct.actionTypes.js
@@ -0,0 +1,5 @@
+export const SINGLEPAGE_GET_LOADING = "singlepage/get/loading"
+
+export const SINGLEPAGE_GET_ERROR = "singlepage/get/error"
+
+export const SINGLEPAGE_GET_SUCCESS = "singlepage/get/success"
