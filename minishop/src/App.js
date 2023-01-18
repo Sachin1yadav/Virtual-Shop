6b402c6f235741152ff76cf8062e89caa87e2b6f
sachin_yadav <sy9084087@gmail.com>
2023-01-18 10:58:52 +0530

    all-day1-code

--- minishop/src/App.js
+++ minishop/src/App.js
@@ -1,12 +1,10 @@
- import Navbar from "./components/Navbar"
- import Footer from "./components/Footer";
+ 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <Navbar/>
-    <Footer/>
+     
     </div>
   );
 }
