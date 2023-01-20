e0d7836e1029128db9fdb6e777cda9fe762e4b0a
unknown <sharma78655533@gmail.com>
2023-01-20 21:23:28 +0530

    day-4

--- minishop/src/Routes/AllRoutes.jsx
+++ minishop/src/Routes/AllRoutes.jsx
@@ -4,8 +4,9 @@ import SinglePage from "../Pages/SingleProduct/SinglePage";
 import Admin from '../admin/adminHome/Admin'
 import AdminLogin from '../admin/adminAuth/AdminLogin'
 import AdminContext from "../admin/adminContext/AdminContext";
-import DSign from"../DemoPagesBySachin/DSign"
-import DLogin from"../DemoPagesBySachin/DLogin"
+import DSign from"../DemoPagesBySachin/DSign";
+import DLogin from"../DemoPagesBySachin/DLogin";
+import Cart from "../Pages/Cart/Cart";
 function Allroute() {
   return (
     <>
@@ -13,7 +14,7 @@ function Allroute() {
         <Route path="/" element={<Home/>} />
         {/* <Route path="/login" element={<Login/>} />
         <Route path="/sign" element={<Sign/>} /> */}
-        {/* <Route path="/cart" element={<Cart/>} /> */}
+         <Route path="/cart" element={<Cart/>} /> 
         {/* <Route path="/products" element={<Products/>}></Route> */}
         <Route path="/data/:id" element={<SinglePage/>}></Route>
         <Route path="/admin" element={<AdminContext><Admin/> </AdminContext> }></Route>
