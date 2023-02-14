5aa85340b00f700ee9986302d60eb8f5d486c572
sumeet.rana <sumeet.rana@innovaccer.com>
2023-02-15 01:26:11 +0530

    don't merge: code review

--- minishop/src/redux/AddUser/User.actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const USER_ADD_SUCCESS = 'user/add/success'
-export const USER_ADD_FAIL = 'user/add/failed' 
--- minishop/src/redux/Order/Order.actionTypes.js
+++
@@ -1,7 +0,0 @@
-export const ORDER_GET_LOADING = "order/get/loading"
-
-export const ORDER_GET_ERROR = "order/get/error"
-
-export const ORDER_GET_SUCCESS = "order/get/success"
-
-export const ORDER_REMOVE = "order/delete"
